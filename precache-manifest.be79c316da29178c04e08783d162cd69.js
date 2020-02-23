1ed65b52cfac3e2fa5d3965e8eed73a0362e44c1
Carq <lciura.pl@gmail.com>
2020-02-23 22:56:44 +0100

    Updates

---
+++ precache-manifest.be79c316da29178c04e08783d162cd69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b696dba63eda5b997ad33c135ba779eb",
+    "url": "/metting-meter/index.html"
+  },
+  {
+    "revision": "ba39afd2ec83d78dcb24",
+    "url": "/metting-meter/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4dbc675919b0d9b9f6a9",
+    "url": "/metting-meter/static/js/2.828f2fbb.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/metting-meter/static/js/2.828f2fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba39afd2ec83d78dcb24",
+    "url": "/metting-meter/static/js/main.a54546cc.chunk.js"
+  },
+  {
+    "revision": "a34c550047364e379337",
+    "url": "/metting-meter/static/js/runtime-main.5c49a5cd.js"
+  }
+]);
